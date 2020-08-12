8cf9e31ba6afa3d29ceb7763c6eac6309298c174
Victoria Rogozhkina <v.rogozhkina@gmail.com>
2020-08-11 22:48:59 -0700

    add card styles

--- src/homework9_kanban/Task.js
+++ src/homework9_kanban/Task.js
@@ -4,23 +4,25 @@ const statuses = ['todo', 'progress', 'review', 'done'];
 
 function Task({task, updateTask, index, tasks}) {
     return (
-        <div>
-            <div>{task.title}</div>
-            <div>{task.description}</div>
-            <div>{task.priority}</div>
-            <div>
-                <button disabled={index === 0} onClick={() => {
-                    task.status = statuses[index - 1];
-                    updateTask()
-                }}>&lt;</button>
-                <button disabled={index === 3} onClick={() => {
-                    task.status = statuses[index + 1];
-                    updateTask()
-                }}>&gt;</button>
-                <button onClick={() => {
-                    tasks.splice(tasks.indexOf(task), 1);
-                    updateTask()
-                }}>&times;</button>
+        <div className='card'>
+            <div className='card-header'>{task.title}</div>
+            <div className='card-body'>
+                <div>{task.description}</div>
+                <div>{task.priority}</div>
+                <div>
+                    <button disabled={index === 0} onClick={() => {
+                        task.status = statuses[index - 1];
+                        updateTask()
+                    }}>&lt;</button>
+                    <button disabled={index === 3} onClick={() => {
+                        task.status = statuses[index + 1];
+                        updateTask()
+                    }}>&gt;</button>
+                    <button onClick={() => {
+                        tasks.splice(tasks.indexOf(task), 1);
+                        updateTask()
+                    }}>&times;</button>
+                </div>
             </div>
         </div>
     );
